78380b3f6c18c7a1bcec710a51c2c0887d229952
xzxedu <xzxtju@outlook.com>
2019-08-23 17:15:23 +0800

    login加db

--- server/application/controllers/Login.php
+++ server/application/controllers/Login.php
@@ -2,6 +2,8 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 use QCloud_WeApp_SDK\Auth\LoginService as LoginService;
 use QCloud_WeApp_SDK\Constants as Constants;
+use QCloud_WeApp_SDK\Mysql\Mysql as DB;
+
 class Login extends CI_Controller {
     public function index() {
         $result = LoginService::login();
